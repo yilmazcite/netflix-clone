4e7a3494b877e823a6de5d3d418fb35403e9ab36
Yılmaz <yilmazcite@gmail.com>
2022-09-30 13:37:28 +0300

    firebase initiated & routes added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between absolute w-full p-4 z-[100]">
-      <h1 className="text-red-600 text-bold text-4xl cursor-pointer">
-        NETFLIX
-      </h1>
+      <Link to="/">
+        <h1 className="text-red-600 text-bold text-4xl cursor-pointer">
+          NETFLIX
+        </h1>
+      </Link>
       <div>
-        <button className="text-white rounded cursor-pointer mr-4">
-          Sign In
-        </button>
-        <button className="text-white bg-red-600 rounded py-2 px-6 cursor-pointer">
-          Sign Up
-        </button>
+        <Link to="/login">
+          <button className="text-white rounded cursor-pointer mr-4">
+            Sign In
+          </button>
+        </Link>
+        <Link to="/signup">
+          <button className="text-white bg-red-600 rounded py-2 px-6 cursor-pointer">
+            Sign Up
+          </button>
+        </Link>
       </div>
     </div>
   );
